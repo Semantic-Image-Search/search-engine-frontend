0f5407332bf093d11d9f8e7a527edd7d42d07b52
Pranav Kumar <17pranav.k@gmail.com>
2022-09-06 18:42:09 +0530

    initialized the frontend and backend app

---
+++ search-engine-frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="search-engine">
+      <h1>Welcome to search engine Unsplash</h1>
+
+    </div>
+  );
+}
+
+export default App;
